551ad21eb4881ee99213a1862def2452d10de31c
apollohpp <apollo.hpp@gmail.com>
2019-05-06 17:06:40 +0900

    Modified path of packages

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,25 +3,25 @@ package main
 import (
 	"fmt"
 	"sort"
-	tiny_elephant_go "tiny-elphant-go"
+	te "tiny-elephant/tiny-elephant"
 )
 
 func main() {
 	data := map[string][]string{
-		"user1": {"airplane", "banana", "cat", "dog", "elephant", "fruit", "google", "hobby", "internet", "jogging"},
-		"user2": {"cat", "dog", "elephant", "fruit", "google", "jogging", "kotlin"},
-		"user3": {"java", "rx", "yahoo", "zoo"},
-		"user4": {"apple", "banana"},
-		"user5": {"airplane"},
-		"user6": {"bobby", "dog"},
-		"user7": {"train", "cat", "exercise", "healthy"},
-		"user8": {"healthy", "dog", "exercise", "banana", "youtube"},
-		"user9": {"java", "javascript", "rx", "zoo", "yahoo", "google", "github"},
+		"user1":  {"airplane", "banana", "cat", "dog", "elephant", "fruit", "google", "hobby", "internet", "jogging"},
+		"user2":  {"cat", "dog", "elephant", "fruit", "google", "jogging", "kotlin"},
+		"user3":  {"java", "rx", "yahoo", "zoo"},
+		"user4":  {"apple", "banana"},
+		"user5":  {"airplane"},
+		"user6":  {"bobby", "dog"},
+		"user7":  {"train", "cat", "exercise", "healthy"},
+		"user8":  {"healthy", "dog", "exercise", "banana", "youtube"},
+		"user9":  {"java", "javascript", "rx", "zoo", "yahoo", "google", "github"},
 		"user10": {"cook", "bobby", "dog", "youtube"},
 		"user11": {"dance", "airplane", "trip", "elephant", "fruit", "google"},
 	}
 
-	imc := tiny_elephant_go.NewInMemoryCluster(
+	imc := te.NewInMemoryCluster(
 		"localhost:6379",
 		1,
 		128,
---
+++ go.mod
@@ -0,0 +1,12 @@
+module tiny-elephant
+
+go 1.12
+
+require (
+	github.com/dgryski/go-metro v0.0.0-20180109044635-280f6062b5bc // indirect
+	github.com/dgryski/go-minhash v0.0.0-20190315135803-ad340ca03076
+	github.com/dgryski/go-spooky v0.0.0-20170606183049-ed3d087f40e2 // indirect
+	github.com/go-redis/redis v6.15.2+incompatible
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
+)
